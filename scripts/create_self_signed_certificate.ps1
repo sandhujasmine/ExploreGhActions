9e0241c7220d2fa80582944ccdd61a445767969f
Jasmine Sandhu <jsandhu@anaconda.com>
2023-06-25 08:04:30 -0700

    test creating secrets

---
+++ scripts/create_self_signed_certificate.ps1
@@ -0,0 +1,15 @@
+# Create a self signed certificate
+$certSplat = @{
+    DnsName = 'constructor@anaconda.com'
+    Type = 'CodeSigning'
+    NotAfter = (Get-Date).AddDays(365)
+    CertStoreLocation = 'cert:\CurrentUser\My'
+}
+$myCert = New-SelfSignedCertificate @certSplat
+$myPwd = ConvertTo-SecureString -String "$env:CONSTRUCTOR_PFX_CERTIFICATE_PASSWORD" -Force -AsPlainText
+$pfxSplat = @{
+    Cert = $myCert
+    FilePath = "$env:CONSTRUCTOR_SIGNING_CERTIFICATE"
+    Password = $myPwd
+}
+Export-PfxCertificate @pfxSplat
