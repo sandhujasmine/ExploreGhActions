78abd5941853e7a4861428075ff54778bdaadfdf
Jasmine <sandhujasmine@users.noreply.github.com>
2023-09-23 08:41:57 -0700

    Test create pr (#29)
    * workflow to try out PR creation
    
    * add python file for adding customer.json
    
    * Create workflow for creating customer.json and a PR

---
+++ scripts/customer_json.py
@@ -0,0 +1,16 @@
+import os
+import json
+from pathlib import Path
+
+
+d_customer = {key.lower(): os.environ[key] for key in ('INSTALLER_NAME',
+                                                       'SVC_BASE_INSTALLER',
+                                                       'CUSTOMER_TITLE',
+                                                       'ORG_ID', 'TOKEN')}
+# Customer slug obtained from customer_title + org_id
+customer_slug = (d_customer['customer_title'].lower().replace(' ', '-') + '_' +
+                 d_customer['token'][:5])
+customer_dir = Path(os.environ['BASE_DIR']) / customer_slug
+customer_dir.mkdir()
+with open(customer_dir/'customer.json', 'w') as fl:
+    json.dump(d_customer, fl, indent=4)
