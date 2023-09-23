517f7fbb86b18ea026464f2a89c1c426cd485607
Jasmine Sandhu <jsandhu@anaconda.com>
2023-09-23 08:41:07 -0700

    Create workflow for creating customer.json and a PR

--- scripts/customer_json.py
+++ scripts/customer_json.py
@@ -6,10 +6,11 @@ from pathlib import Path
 d_customer = {key.lower(): os.environ[key] for key in ('INSTALLER_NAME',
                                                        'SVC_BASE_INSTALLER',
                                                        'CUSTOMER_TITLE',
-                                                       'CUSTOMER_SLUG',
                                                        'ORG_ID', 'TOKEN')}
-
-customer_dir = Path(os.environ['BASE_DIR']) / d_customer.pop('customer_slug')
+# Customer slug obtained from customer_title + org_id
+customer_slug = (d_customer['customer_title'].lower().replace(' ', '-') + '_' +
+                 d_customer['token'][:5])
+customer_dir = Path(os.environ['BASE_DIR']) / customer_slug
 customer_dir.mkdir()
 with open(customer_dir/'customer.json', 'w') as fl:
     json.dump(d_customer, fl, indent=4)
